3e958e68491c8b6f75a58d6aac23991320fbb223
Shanmugavel <iashanmugavel@gmail.com>
2015-07-21 22:04:38 +0530

    created rails-gem-example-project

---
+++ rails-gem-example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsGemExample::Application.config.secret_token = '58b1546b9a4d31c4268f1c98c8dbf27e8b7ee1c1bb7a197b8091834fe9b0f076c58eed8c57855331bed79bfe142be9ae688ed2fbae50ca8600448ab7bca587e2'
