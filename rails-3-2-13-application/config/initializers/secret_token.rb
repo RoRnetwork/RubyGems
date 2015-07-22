8d34ecc2b43790d4aa446c9d28486aa3de4f1489
Shanmugavel <iashanmugavel@gmail.com>
2015-07-23 00:37:06 +0530

    created rails 3.2.13 application

---
+++ rails-3-2-13-application/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3213Application::Application.config.secret_token = '39bd523bf06e1c29db79bb2ad63aa6fcd6122a01398c1c4898352716a4a595b6aeedae8e6fd5c1168846c572c0103213e8e93d605b2a3fd751c8011b81ec842b'
