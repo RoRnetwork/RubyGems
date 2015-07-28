47e336ae7b8d85341e9a472d96e52243a14965ad
Shanmugavel <iashanmugavel@gmail.com>
2015-07-29 00:48:42 +0530

    created ruby1.9.3 and rails 3.2.13 project with devise 2.2.7

---
+++ upgrading-devise-gem/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UpgradingDeviseGem::Application.config.secret_token = 'cbaf9ecda3166a772f3fa7d14501f92babc343b5a149addf9811768768520b4ad7348c857fcfce0be9292bdab339ad087e38622bc1d69fc98efb484c9e5faa95'
